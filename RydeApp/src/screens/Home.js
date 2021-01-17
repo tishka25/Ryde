d4505ff71938f6566bc01c619edfc7204cd00d09
tishka25 <teodorstanishev@gmail.com>
2021-01-17 19:34:21 +0200

    add offer button

--- RydeApp/src/screens/Home.js
+++ RydeApp/src/screens/Home.js
@@ -8,8 +8,10 @@ import {
     Text,
     StatusBar,
 } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import TravelList from '../components/TravelList';
 import requestHandler from '../utils/requestHandler';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 const Home = (props) => {
@@ -36,11 +38,33 @@ const Home = (props) => {
             <StatusBar barStyle="dark-content" />
             <View style={{ width: "100%", height: "100%" }}>
                 <TravelList {...props} offers={offers}/>
+                {/* Add ofer */}
+                <View style={styles.addOfferContainer}>
+                    <TouchableOpacity>
+                        <Icon name={"plus"} size={24} color={"white"}/>
+                    </TouchableOpacity>
+                </View>
+                {/*  */}
             </View>
 
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    addOfferContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        bottom: 8,
+        right: 8,
+        width: 58,
+        height: 58,
+        borderRadius: 100,
+        backgroundColor: "#987bf3"
+    }
+});
 
 
 export default Home;
